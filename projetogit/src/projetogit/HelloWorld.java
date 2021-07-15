95e6833257d0cd3887ed0df5b54d7c89801c1beb
Madson <madsonsmitt@gmail.com>
2021-07-15 20:52:24 -0300

    commit pratica teste

--- projetogit/src/projetogit/HelloWorld.java
+++ projetogit/src/projetogit/HelloWorld.java
@@ -4,5 +4,6 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("hello world!3");
 		System.out.println("master + hotfix");
+		System.out.println("commit teste");
 	}
 }
